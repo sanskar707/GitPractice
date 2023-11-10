59eeaa5bdde8a0d231d077295f9a0f569693f506
sanskar707 <sanskarmate404@gmail.com>
2023-11-10 21:30:18 +0530

    added

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ for(let i = 1; i <= 10; i++){
 }
 
 console.log("EcLiPsE")
+
+let sanskar=(x,y)=>{
+    return x - y
+}
+let hie = [1, 34, 35, 7, 4, 3, 2, 56, 39]
+hie.sort(sanskar)
+console.log(hie)
